325a01d337e9981142dfefc7c9050b08ae38f410
Stephen Ghiandoni <stephenmghiandoni@gmail.com>
2023-07-31 19:59:11 -0400

    received params in array format from db

--- sh/appraise.sh
+++ sh/appraise.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 #This script is executed by display_games.php when the user wants to appraise the list of games in collection
-#process query passed one by one, download webpage and parse out value
-#dependencies: lynx, jq
-#$1 = number of games, $2 = json object of games {url,title,game,box,manual,factory_sealed}
+#dependencies: lynx
 
 TMP_DIR="/var/www/html/sh/tmp"
 LOCK_FILE="$TMP_DIR/appraise.lock"
 num_games=$1
-urls=$2
+gameid_list=($2) #convert params back into arrays 
+query_list=($3)
+game_list=($4)
+box_list=($5)
+manual_list=($6)
+sealed_list=($7)
 
 
 #filename=$(echo "$title.txt" | sed 's/\//-/g' )
 
+#prevent script from running more than once
 if [ -f "$LOCK_FILE" ]; then
 	printf "Error: appraisal in progress..."
 	exit 1
@@ -19,24 +23,22 @@ fi
 
 printf "Process $$ is running." > "$LOCK_FILE"
 
-printf "Arrays:</br>"
+for (( i=0; i<$num_games; i++ ))
+do
+	#echo ${gameid_list[$i]}
+	#download page from url
+	#full_url="https://www.pricecharting.com/game/$title"
+	#printf "Downloading $url...</br>"
+	#printf "filename: $filename</br>"
+	#lynx -dump "$url" > $TMP_DIR/$filename
 
+	#parse data to extract price info
+	#price_list=$(cat $TMP_DIR/$filename | grep [$][0-9][0-9] | grep '^[^[:alpha:]]*$')#redo this, it's not right
+	#echo $price_list
 
-for arg in "$@"; do
-	echo "$arg</br>"
+	#perform sql insert
+
 done
-
-#download webpage
-#full_url="https://www.pricecharting.com/game/$title"
-#printf "Downloading $url...</br>"
-#printf "filename: $filename</br>"
-#lynx -dump "$url" > $TMP_DIR/$filename
-
-##parse data to extract price info
-#price_list=$(cat $TMP_DIR/$filename | grep [$][0-9][0-9] | grep '^[^[:alpha:]]*$')#redo this, it's not right
-#echo $price_list
-
-##when finished scraping, insert price data into db
 
 rm -f "$LOCK_FILE"
 
