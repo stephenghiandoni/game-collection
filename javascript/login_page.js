1c85a9868581137587e993fa1986a02552a989ae
Stephen Ghiandoni <stephenmghiandoni@gmail.com>
2024-03-02 16:00:38 -0500

    Checking for valid username and password

--- javascript/login_page.js
+++ javascript/login_page.js
@@ -1,3 +1,41 @@
+const pw_length = 6;//minimum password length
+const username = document.getElementById('new_uname');
+const password = document.getElementById('new_pword');
+const confirm_password = document.getElementById('confirm_new_pword');
+const submit_button = document.getElementById('submit_new_account');
+
+let valid_uname = false;
+let valid_pword = false;
+
+username.addEventListener('input', function(event){
+	const text = this.value;
+	if(text.trim() != '') valid_uname = true;
+	else valid_uname = false;
+	check_validity();
+});
+
+function password_check_event_handler(event){
+	const pw_text = password.value;
+	const pw_text_length = pw_text.length;
+	const confirm_pw_text = confirm_password.value;
+
+	if(pw_text_length >= pw_length && pw_text == confirm_pw_text) valid_pword = true;
+	else valid_pword = false;
+	check_validity();
+
+}
+
+password.addEventListener('input', password_check_event_handler);
+confirm_password.addEventListener('input', password_check_event_handler);
+
+function check_validity(){
+	if(valid_uname && valid_pword){
+		submit_button.disabled = false;		
+	}else{
+		submit_button.disabled = true;
+	}
+}
+
 function toggle_signup_form(){
 	event.preventDefault();
 	console.log("function called");
@@ -15,3 +53,4 @@ function toggle_signup_form(){
 		login.style.display = "none";
 	}
 }
+
